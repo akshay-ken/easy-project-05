2d27403d1072d603a422e361edc6950a69957e3d
akshay-ken <akshaybest3120@gmail.com>
2025-06-04 15:05:23 +0530

    first changes

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="font-Han font-bold ">working</h1>
+      <h1>working</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/SingleCard.jsx
@@ -0,0 +1,7 @@
+export function SingleCard() {
+  return (
+    <>
+      <main></main>
+    </>
+  );
+}
